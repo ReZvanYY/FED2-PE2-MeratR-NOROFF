a2f57eda3e10b528660a9be04bc27cd188edea48
Merat R <merat_agha@hotmail.com>
2026-05-24 11:40:10 +0200

    Update App.tsx
    I have now added every route to the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Register from "./pages/RegisterPage";
+import Login from "./pages/LoginPage";
+import Home from "./pages/HomePage";
+import DetailedVenuePage from "./pages/DetailedVenuePage";
+import CheckoutPage from "./pages/CheckoutPage";
+import BookingConfirmation from "./pages/BookingConfirmation";
+import ProfilePage from "./pages/ProfilePage";
+import BecomeVenueManager from "./pages/BecomeVenueManager";
+import VenueManagerDashboard from "./pages/VenueManagerDashboard";
+import CreateVenuePage from "./pages/CreateVenuePage";
+import EditVenuePage from "./pages/EditVenuePage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import TermsAndConditionsPage from "./pages/UserTerms";
+import HostTermsPage from "./pages/HostsTerms";
+import PrivacyPolicyPage from "./pages/PrivacyPolicy";
 
-function App() {
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/venues/:id" element={<DetailedVenuePage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/booking-confirmation" element={<BookingConfirmation />}/>
+          <Route path="/profile" element={<ProfilePage />}/>
+          <Route path="/become-a-venue-manager" element={<BecomeVenueManager />}/>
+          <Route path="/dashboard" element={<VenueManagerDashboard />}/>
+          <Route path="/create-venue" element={<CreateVenuePage />}/>
+          <Route path="/edit-venue/:id" element={<EditVenuePage />}/>
+          <Route path="/contact" element={<ContactPage />}/>
+          <Route path="/about" element={<AboutPage />}/>
+          <Route path="/user-terms" element={<TermsAndConditionsPage />}/>
+          <Route path="/host-terms" element={<HostTermsPage />}/>
+          <Route path="/privacy" element={<PrivacyPolicyPage />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
